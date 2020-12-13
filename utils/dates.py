a16cff16986a97645ce9991b8b0b982ffa1b9e9f
az365 <artemcarr@yandex.ru>
2020-12-13 22:37:05 +0300

    get_month_from_date(), get_next_week_date(), get_weeks_range() added

--- utils/dates.py
+++ utils/dates.py
@@ -1,5 +1,11 @@
 from datetime import date, timedelta
 
+try:  # Assume we're a sub-module in a package.
+    from utils import arguments as arg
+except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
+    from ..utils import arguments as arg
+
+
 DAYS_IN_WEEK = 7
 WEEKS_IN_YEAR = 52
 
@@ -51,6 +57,15 @@ def get_month_first_date(d):
         raise_date_type_error(d)
 
 
+def get_month_from_date(d):
+    if check_iso_date(d):
+        return date.fromisoformat(d).month
+    elif isinstance(d, date):
+        return d.month
+    else:
+        raise_date_type_error(c)
+
+
 def get_monday_date(d, as_iso_date=None):
     cur_date = get_date(d)
     if as_iso_date is None:
@@ -72,10 +87,40 @@ def get_next_year_date(d, increment=1, round_to_monday=False):
         dt = '{:04}-{:02}-{:02}'.format(dt.year + increment, dt.month, dt.day)
     elif isinstance(d, date):
         dt = date(d.year + increment, d.month, d.day)
+    else:
+        raise_date_type_error(d)
     if round_to_monday:
         return get_monday_date(dt, is_iso_format)
     else:
         return dt
+
+
+def get_next_week_date(d, increment=1, round_to_monday=False):
+    is_iso_format = check_iso_date(d)
+    if is_iso_format:
+        dt = date.friomisoformat(d)
+    elif isinstance(d, date):
+        dt = d
+    else:
+        raise_date_type_error(d)
+    dt += timedelta(days=DAYS_IN_WEEK * increment)
+    if round_to_monday:
+        dt = get_monday_date(d)
+    if is_iso_format:
+        return to_date(dt, is_iso_format)
+    else:
+        return dt
+
+
+def get_weeks_range(date_min, date_max):
+    weeks_range = list()
+    cur_date = get_monday_date(date_min)
+    if cur_date < date_min:
+        cur_date = get_next_week_date(cur_date, increment=1)
+    while cur_date <= date_max:
+        weeks_range.append(cur_date)
+        cur_date = get_next_week_date(cur_date, increment=1)
+    return weeks_range
 
 
 def get_weeks_between(a, b, round_to_mondays=False):
@@ -108,12 +153,13 @@ def get_year_and_week_from_date(d):
     return year, week
 
 
-def get_week_abs_from_year_and_week(year, week, min_year=MIN_YEAR):
+def get_week_abs_from_year_and_week(year, week, min_year=arg.DEFAULT):
+    min_year = arg.undefault(min_year, MIN_YEAR)
     week_abs = (year - min_year) * WEEKS_IN_YEAR + week
     return week_abs
 
 
-def get_week_abs_from_date(d, min_year=MIN_YEAR):
+def get_week_abs_from_date(d, min_year=arg.DEFAULT):
     year, week = get_year_and_week_from_date(d)
     week_abs = get_week_abs_from_year_and_week(year, week, min_year=min_year)
     return week_abs
@@ -124,15 +170,15 @@ def get_week_no_from_date(d):
     return week_no
 
 
-def get_year_and_week_from_week_abs(week_abs, min_year=MIN_YEAR):
+def get_year_and_week_from_week_abs(week_abs, min_year=arg.DEFAULT):
+    min_year = arg.undefault(min_year, MIN_YEAR)
     delta_year = int(week_abs / WEEKS_IN_YEAR)
     year = min_year + delta_year
     week = week_abs - delta_year * WEEKS_IN_YEAR
     return year, week
 
 
-def get_date_from_week_abs(week_abs, min_year=MIN_YEAR, as_iso_date=True):
+def get_date_from_week_abs(week_abs, min_year=arg.DEFAULT, as_iso_date=True):
     year, week = get_year_and_week_from_week_abs(week_abs, min_year=min_year)
     cur_date = get_date_from_year_and_week(year, week, as_iso_date=as_iso_date)
     return cur_date
-
