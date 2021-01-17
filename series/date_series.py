3d3dea64d120cf2f7732fb181b49a525700c085d
az365 <artemcarr@yandex.ru>
2021-01-17 16:04:19 +0300

    DateSeries: get_nearest_date(), get_segment_for_date() methods added

--- series/date_series.py
+++ series/date_series.py
@@ -11,8 +11,8 @@ except ImportError:  # Apparently no higher-level package has been imported, fal
 class DateSeries(PairSeries):
     def __init__(
             self,
-            dates,
-            values,
+            dates=[],
+            values=[],
             is_sorted=False,
     ):
         super().__init__(
@@ -41,38 +41,219 @@ class DateSeries(PairSeries):
             return max(self.get_dates())
 
     def get_period_days(self):
-        raise NotImplemented
+        return dt.get_days_between(
+            self.get_first_date(),
+            self.get_last_date(),
+        )
 
     def exclude(self, first_date, last_date):
-        raise NotImplemented
+        return self.filter_keys(lambda d: d < first_date or d > last_date)
 
     def period(self, first_date, last_date):
-        raise NotImplemented
+        return self.filter_keys(lambda d: first_date <= d <= last_date)
 
     def cropped(self, left_days, right_days):
-        raise NotImplemented
+        return self.period(
+            dt.get_shifted_date(self.get_first_date(), days=abs(left_days)),
+            dt.get_shifted_date(self.get_last_date(), days=-abs(right_days)),
+        )
 
     def first_year(self):
-        raise NotImplemented
+        date_a = self.get_first_date()
+        date_b = dt.get_next_year_date(date_a)
+        return self.period(date_a, date_b)
 
     def last_year(self):
-        raise NotImplemented
+        date_b = self.get_last_date()
+        date_a = dt.get_next_year_date(date_b, increment=-1)
+        return self.period(date_a, date_b)
 
     def shift(self, distance):
-        raise NotImplemented
+        return self.map_keys(lambda d: dt.get_shifted_date(d, distance))
 
-    def yearly_shift(self, return_series=True):
-        for n in range(self.get_count()):
-            self.values[n] = dt.get_next_year_date(self.values[n])
-        if return_series:
-            return self
+    def yearly_shift(self):
+        return self.map_keys(dt.get_next_year_date())
 
     def round_to_months(self):
         self.map_keys(dt.get_month_first_date).mean_by_keys()
 
     def get_distance_series(self, date, take_abs=True):
-        raise NotImplemented
+        distance_series = self.__class__(
+            self.key_series(),
+            self.key_series().map(lambda d: dt.get_days_between(date, d)),
+            is_sorted=self.is_sorted,
+        )
+        if take_abs:
+            distance_series.map_values(abs)
+        return distance_series
 
     def get_nearest_date(self, date):
+        if self.get_count() == 0:
+            return None
+        elif self.get_count() == 1:
+            return self.get_first_key()
+        elif self.is_sorted:
+            prev_date = None
+            prev_distance = None
+            for cur_date in self.get_keys():
+                if cur_date == date:
+                    return cur_date
+                else:
+                    cur_distance = abs(dt.get_days_between(cur_date, date))
+                    if prev_distance is not None:
+                        if cur_distance > prev_distance:
+                            return prev_date
+                    prev_date = cur_date
+                    prev_distance = cur_distance
+            return cur_date
+        else:
+            return self.get_distance_series(date, take_abs=True).get_arg_min()
+
+    def get_two_nearest_dates(self, date):
+        if self.get_count() < 2:
+            return None
+        else:
+            distance_series = self.get_distance_series(date, take_abs=False)
+            if not distance_series.is_sorted:
+                distance_series = distance_series.sort_by_keys()
+            date_a = distance_series.filter_values(lambda v: v < 0).get_arg_max()
+            date_b = distance_series.filter_values(lambda v: v >= 0).get_arg_min()
+            return date_a, date_b
+
+    def get_segment_for_date(self, date):
+        nearest_dates = [i for i in self.get_two_nearest_dates(date) if i]
+        return self.__class__.from_items(
+            [(d, self.get_value(d)) for d in nearest_dates],
+            is_sorted=True,
+        )
+
+    def get_linear_interpolated_value(self, date, near_for_outside=True):
+        segment = self.get_segment_for_date(date)
+        if segment.get_count() == 1:
+            if near_for_outside:
+                return segment.get_first_value()
+        elif segment.get_count() == 2:
+            [(date_a, value_a), (date_b, value_b)] = segment.get_list()
+            segment_days = segment.get_period_days()
+            distance_days = dt.get_days_between(date_a, date)
+            interpolated_value = value_a + (value_b - value_a) * distance_days / segment_days
+            return interpolated_value
+
+    def get_spline_interpolated_value(self, date):
         raise NotImplemented
 
+    def get_interpolated_value(self, date, use_spline=False):
+        value = self.get_value(date)
+        if value:
+            return value
+        if use_spline:
+            return self.get_spline_interpolated_value(date)
+        else:
+            return self.get_linear_interpolated_value(date)
+
+    def interpolate(self, dates, use_spline=False):
+        result = self.new()
+        for d in dates:
+            result.append_pair(d, self.get_interpolated_value(d, use_spline=use_spline))
+        if dates == sorted(dates):
+            result.is_sorted = True
+        return result
+
+    def interpolate_to_weeks(self, use_spline=True):
+        monday_dates = dt.get_weeks_range(self.get_first_date(), self.get_last_date())
+        return self.interpolate(monday_dates, use_spline=use_spline)
+
+    def apply_window_series_function(
+            self,
+            window_days_count,
+            function,
+            input_as_dict=False,
+            for_full_window_only=False,
+    ):
+        half_window_days = window_days_count / 2
+        int_half_window_days = int(half_window_days)
+        window_days_is_even = half_window_days == int_half_window_days
+        left_days = int_half_window_days
+        right_days = int_half_window_days if window_days_is_even else int_half_window_days + 1
+        result = self.new()
+        if for_full_window_only:
+            dates = self.cropped(left_days, right_days).get_dates()
+        else:
+            dates = self.get_dates()
+        for center_date in dates:
+            window - self.period(
+                dt.get_shifted_date(d, -left_days),
+                dt.get_shifted_date(d, right_days),
+            )
+            if input_as_dict:
+                window = window.get_dict()
+            result.append_pair(d, function(window))
+        return result
+
+    def apply_interpolated_window_series_function(
+            self,
+            window_days_list,
+            function,
+            input_as_list=False,
+            for_full_window_only=False,
+    ):
+        result = self.new()
+        left_days = min(window_days_list)
+        right_days = max(window_days_list)
+        if for_full_window_only:
+            dates = self.cropped(left_days, right_days).get_dates()
+        else:
+            dates = self.get_dates()
+        for d in dates:
+            window_dates = [dt.get_shifted_date(d, days) for days in window_days_list]
+            window = self.interpolate(window_dates)
+            window_values = window.get_values()
+            if None not in window_values or not for_full_window_only:
+                if input_as_list:
+                    window = window_values
+                result.append_pair(d, function(window))
+        return result
+
+    def smooth(self, window_days_list=[-7, 0, 7]):
+        return self.apply_interpolated_window_series_function(
+            window_days_list=window_days_list,
+            function=lambda s: s.get_mean(),
+            input_as_list=False,
+            for_full_window_only=False,
+        )
+
+    def math(self, series, function, use_spline=False):
+        assert isinstance(series, DateSeries)
+        result = self.new()
+        for d, v in self.get_items():
+            if v is not None:
+                v0 = series.get_interpolated_value(d, use_spline=use_spline)
+                if v0 is not None:
+                    result.append(d, function(v, v0))
+        return result
+
+    def yoy(self, use_spline=False):
+        yearly_shifted = self.yearly_shift()
+        return self.math(
+            yearly_shifted,
+            function=lambda a, b: (a - b) / b if b else None,
+            use_spline=use_spline,
+        )
+
+    def get_yoy_for_date(self, date, use_spline=False):
+        if not self.cached_yoy:
+            self.cached_yoy = self.yoy(use_spline=use_spline)
+        if date in self.cached_yoy:
+            return self.get_value(date)
+        elif date < self.get_first_date():
+            return self.first_year().get_mean()
+        elif date > self.get_last_date():
+            return self.last_year().get_mean()
+        else:
+            return self.get_interpolated_value(date, use_spline=use_spline)
+
+    def extrapolate_by_yoy(self, date_min, date_max):
+        raise NotImplemented
+
+    def extrapolate(self, date_min, date_max):
+        raise NotImplemented
--- series/test.py
+++ series/test.py
@@ -1,13 +1,9 @@
 try:  # Assume we're a sub-module in a package.
-    from series.any_series import (
-        AnySeries,
-        is_defined,
-    )
+    from series.any_series import AnySeries
+    from series.date_series import DateSeries
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
-    from ..series.any_series import (
-        AnySeries,
-        is_defined,
-    )
+    from ..series.any_series import AnySeries
+    from ..series.date_series import DateSeries
 
 
 def test_smooth():
@@ -17,5 +13,36 @@ def test_smooth():
     assert received == expected
 
 
+def test_get_nearest_date():
+    data = {'2020-01-01': 10, '2021-01-01': 20}
+    cases = ['2019-12-01', '2020-02-01', '2020-12-01', '2021-12-02']
+    expected = ['2020-01-01', '2020-01-01', '2021-01-01', '2021-01-01']
+    received = [DateSeries.from_dict(data).get_nearest_date(d) for d in cases]
+    assert received == expected
+
+
+def test_get_segment_for_date():
+    data = {'2020-01-01': 10, '2021-01-01': 20, '2022-01-01': 30}
+    cases = ['2019-12-01', '2020-02-01', '2021-02-01']
+    expected = [
+        [('2020-01-01', 10)],
+        [('2020-01-01', 10), ('2021-01-01', 20)],
+        [('2021-01-01', 20), ('2022-01-01', 30)],
+    ]
+    received = [DateSeries.from_dict(data).get_segment_for_date(d).get_list() for d in cases]
+    assert received == expected
+
+
+def test_get_interpolated_value():
+    data = {'2019-01-01': 375, '2020-01-01': 10}
+    cases = ['2018-12-01', '2019-02-01', '2019-12-01', '2020-12-02']
+    expected = [375, 344, 41, 10]
+    received = [DateSeries.from_dict(data).get_interpolated_value(d) for d in cases]
+    assert received == expected
+
+
 if __name__ == '__main__':
     test_smooth()
+    test_get_nearest_date()
+    test_get_segment_for_date()
+    test_get_interpolated_value()
